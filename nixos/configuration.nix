54aa9e748e788fbf1a8e0e3fea4e3a028c65f9fc
Donthireddy <donthireddy@yahoo.com>
2024-05-26 09:56:57 -0400

    .

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,8 +7,6 @@
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
   ];
-
-
   nixpkgs = {
     # You can add overlays here
     overlays = [
@@ -202,6 +200,12 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  #
+  # TODO: Should really import this from nginx.nix but nixos-rebuild doesn't find the file
+  #
+  #modules = [
+  #  ./nginx.nix
+  #];
   services.nginx = {
     enable = true;
     virtualHosts."localhost" = {
@@ -227,41 +231,43 @@
   services.samba = {
     enable = true;
     securityType = "user";
-    openFirewall = true;
+    # openFirewall = true;
     extraConfig = ''
-      workgroup = WORKGROUP
+      workgroup = WORKGROUPNIX
       server string = smbnix
+      server role = standalone server
       netbios name = smbnix
-      security = user
-      #use sendfile = yes
-      #max protocol = smb2
-      # note: localhost is the ipv6 localhost ::1
-      # hosts allow = 192.168.0. 127.0.0.1 localhost
-      # hosts deny = 0.0.0.0/0
-      guest account = nobody
-      map to guest = bad user
+      ; security = user
+      ;use sendfile = yes
+      ;max protocol = smb2
+      ; note: localhost is the ipv6 localhost ::1
+      ; hosts allow = 192.168.0. 127.0.0.1 localhost
+      ; hosts deny = 0.0.0.0/0
+      ;guest account = nobody
+      ;map to guest = bad user
     '';
     shares = {
-      public = {
+      twr = {
+        comment = "Tewerubbeth drive";
         path = "/var/www/donthireddy.us/tewerubbeth";
         browseable = "yes";
         "read only" = "no";
-        "guest ok" = "yes";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-        "force user" = "username";
-        "force group" = "groupname";
+        #"guest ok" = "yes";
+        #"create mask" = "0644";
+        #"directory mask" = "0755";
+        #"force user" = "username";
+        #"force group" = "groupname";
       };
-      private = {
-        path = "/home/murali";
-        browseable = "yes";
-        "read only" = "no";
-        "guest ok" = "no";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-        "force user" = "username";
-        "force group" = "groupname";
-      };
+      #private = {
+      #  path = "/home/murali";
+      #  browseable = "yes";
+      #  "read only" = "no";
+      #  "guest ok" = "no";
+      #  "create mask" = "0644";
+      #  "directory mask" = "0755";
+      #  "force user" = "username";
+      #  "force group" = "groupname";
+      #};
     };
   };
 
